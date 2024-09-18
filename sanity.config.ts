7a7078c52e227394a957b5a9060d99781c367cc2
Melody <442283045@qq.com>
2024-09-18 18:43:27 +0800

    feat: 自定义网站

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,7 +5,7 @@
 import { codeInput } from '@sanity/code-input'
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
+import { structure } from 'sanity/structure'
 import { media } from 'sanity-plugin-media'
 
 import { settingsPlugin, settingsStructure } from '~/sanity/plugins/settings'
@@ -24,7 +24,7 @@ export default defineConfig({
   plugins: [
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
-    structureTool({ structure: settingsStructure(settingsType) }),
+    structure({ structure: settingsStructure(settingsType) }),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
